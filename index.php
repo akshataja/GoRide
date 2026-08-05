0c5e30d400ed13b632856b23ee12c5a29fa279b4
akshataja <akshataja6@gmail.com>
2026-08-06 01:10:36 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+include("includes/header.php");
+?>
+
+<div class="hero">
+
+    <h1>Welcome to GoRide</h1>
+
+    <p>Vehicle Rental Booking System</p>
+
+    <a href="vehicles.php">View Vehicles</a>
+
+</div>
+
+<?php
+include("includes/footer.php");
+?>
