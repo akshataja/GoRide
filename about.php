fc2411b47f7254088298920dbaeb2b9defce40da
akshataja <akshataja6@gmail.com>
2026-08-06 14:11:37 +0530

    Create about.php

---
+++ about.php
@@ -0,0 +1,16 @@
+<?php
+include("includes/header.php");
+?>
+
+<h2>About GoRide</h2>
+
+<p>
+GoRide is a simple Vehicle Rental Booking System developed using
+HTML, CSS, JavaScript, PHP and MySQL.
+
+Users can register, login, view vehicles and book vehicles online.
+</p>
+
+<?php
+include("includes/footer.php");
+?>
