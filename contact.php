d3134d9b3061188e19b39007c6d54ca5ff76eb5f
akshataja <akshataja6@gmail.com>
2026-08-06 14:12:47 +0530

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,17 @@
+<?php
+include("includes/header.php");
+?>
+
+<h2>Contact Us</h2>
+
+<p><b>Company :</b> GoRide Vehicle Rental</p>
+
+<p><b>Email :</b> goride@gmail.com</p>
+
+<p><b>Phone :</b> +91 9876543210</p>
+
+<p><b>Address :</b> Jamnagar, Gujarat</p>
+
+<?php
+include("includes/footer.php");
+?>
